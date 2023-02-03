8f4d09023496ec9c13b199fae710da0167ba6763
Oleg Arshinskii <velet592@gmail.com>
2023-02-03 19:48:42 +0200

    Add jieba tokenization

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,35 @@
-use axum::{routing::post, Json, Router};
+use axum::{extract::State, routing::post, Json, Router};
+use jieba_rs::Jieba;
 use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, sync::Arc};
 
 #[tokio::main]
 async fn main() {
-    // initialize tracing
     tracing_subscriber::fmt::init();
 
-    let client = reqwest::Client::new();
+    let jieba = Arc::new(Jieba::new());
 
-    // build our application with a route
-    let app = Router::new().route("/tokenize", post(parse));
+    let app = Router::new()
+        .route("/tokenize", post(tokenize))
+        .with_state(jieba);
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    tracing::debug!("listening on {}", addr);
+    tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn parse(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
+async fn tokenize(
+    State(jieba): State<Arc<Jieba>>,
     Json(payload): Json<ParseRequest>,
 ) -> Json<ParseResponse> {
-    
-    client
+    let fine = vec![jieba
+        .cut(&payload.text, true)
+        .iter()
+        .map(|s| s.to_string())
+        .collect()];
     Json(ParseResponse { fine })
 }
 
@@ -39,17 +40,5 @@ struct ParseRequest {
 
 #[derive(Serialize)]
 struct ParseResponse {
-    fine: Vec<Vec<String>>,
-}
-
-#[derive(Serialize)]
-struct HanLPParseRequest {
-    language: String,
-    text: String,
-}
-
-#[derive(Deserialize)]
-struct HanLPParseResponse {
-    #[serde(rename = "tok/fine")]
     fine: Vec<Vec<String>>,
 }
